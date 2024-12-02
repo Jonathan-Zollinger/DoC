ee230cb899826a73936bf2ab6835fb15a7eb059b
Jonathan Zollinger <jonathan.zollinger@gmail.com>
2024-12-01 22:50:48 -0700

    add micronaut launch template

---
+++ src/main/kotlin/com/jonathan_zollinger/Application.kt
@@ -0,0 +1,7 @@
+package com.jonathan_zollinger
+
+import io.micronaut.runtime.Micronaut.run
+fun main(args: Array<String>) {
+	run(*args)
+}
+
---
+++ src/test/kotlin/com/jonathan_zollinger/DoCTest.kt
@@ -0,0 +1,12 @@
+package com.jonathan_zollinger
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.kotest5.annotation.MicronautTest
+import io.kotest.core.spec.style.StringSpec
+
+@MicronautTest
+class DoCTest(private val application: EmbeddedApplication<*>): StringSpec({
+
+    "test the server is running" {
+        assert(application.isRunning)
+    }
+})
